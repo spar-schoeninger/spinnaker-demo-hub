149fc60334bc18ac009af4546f926b6c0d0693b6
spar-schoeninger <35193063+spar-schoeninger@users.noreply.github.com>
2018-01-07 17:20:40 +0100

    Update main.go

--- main.go
+++ main.go
@@ -15,15 +15,16 @@ func main() {
 func MyPrimeHandler(h http.Handler) http.Handler {
   return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
     // Create some CPU usage
-    var number = 49979687
-    var isPrime = IsPrime(number)
-    log.Printf("Is %v prime? %v\n", number, isPrime)
+    var number = 100
+    var fib = fibonacci(number)
+    log.Printf("%v is the %vth fibonacci number\n", fib, number)
     
     // Serve HTTP
     h.ServeHTTP(w, r)
   })
 }
 
+// Checks, if a given number is prime
 func IsPrime(value int) bool {
     for i := 2; i <= int(math.Floor(float64(value) / 2)); i++ {
         if value % i == 0 {
@@ -31,4 +32,13 @@ func IsPrime(value int) bool {
         }
     }
     return value > 1
+}
+
+// Calculates the n-th fibonacci number
+func fibonacci(n int) int {
+    current, prev := 0, 1
+    for i := 0; i < n; i++ {
+        current, prev = current + prev, current
+    }
+    return current
 }
