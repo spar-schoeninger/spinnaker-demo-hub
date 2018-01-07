bcfd00bfbb32791ef745f5253fef095e46671eef
SCHÖNINGER David (ICS480-SW) <David.SCHOeNINGER@spar-ics.com>
2018-01-07 17:03:12 +0100

    calculating prime number before serving http static content

--- main.go
+++ main.go
@@ -7,15 +7,26 @@ import (
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/", fs)
-	log.Println("Listening...")
-	http.ListenAndServe(":80", nil)
+	fileHandler := http.FileServer(http.Dir("static"))
+	wrappedHandler := MyPrimeHandler(fileHandler)
+	http.ListenAndServe(":80", wrappedHandler)
+}
+
+func MyPrimeHandler(h http.Handler) http.Handler {
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    // Create some CPU usage
+    var number = 49979687
+    var isPrime = IsPrime(number)
+    log.Printf("Is %v prime? %v\n", number, isPrime)
+    
+    // Serve HTTP
+    h.ServeHTTP(w, r)
+  })
 }
 
 func IsPrime(value int) bool {
     for i := 2; i <= int(math.Floor(float64(value) / 2)); i++ {
-        if value%i == 0 {
+        if value % i == 0 {
             return false
         }
     }
